2fc8046dfad811157b3bf8c719ca142002b616af
mpoma-dash <125382297+mpoma-dash@users.noreply.github.com>
2024-10-07 10:41:03 -0500

    Update Dash_Recruitment_App.py

--- Dash_Recruitment_App.py
+++ Dash_Recruitment_App.py
@@ -596,7 +596,10 @@ def ncaa_positional_zscore_df(state):
         player_data[col_list] = player_data[col_list].apply(zscore)
         player_data['Average_ZScore'] = player_data[col_list].mean(axis=1)
         player_data = player_data.drop(columns=['index'])
+        player_data = player_data.sort_values(by='Average_ZScore', ascending=False)
         player_data = player_data.set_index(['Team', 'Player', 'Average_ZScore'])
+        
+        
         cf_df = (player_data.style.background_gradient(vmin=-3, vmax=3,
                                                        cmap=sns.color_palette("seismic_r", as_cmap=True),
                                                        subset=col_list))
